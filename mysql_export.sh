1289c47d8d96efbda1a8cf32f1fc49ca84a54635
Pat Marion <pat.marion@drive.ai>
2018-07-22 18:08:06 -0700

    add chmod command to mysql_export.sh

--- mysql_export.sh
+++ mysql_export.sh
@@ -3,4 +3,6 @@ set -x
 
 /etc/init.d/mysql start
 mysqldump -u root --databases cdash | gzip > /root/local_files/cdash_database.sql.gz
+chmod og+w /root/local_files/cdash_database.sql.gz
+
 echo Finished.
